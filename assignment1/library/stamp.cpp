42db7229ce1cb04684ffcf92ca0fbbe040ad3c0b
HarshitJain-1908 <88964469+HarshitJain-1908@users.noreply.github.com>
2023-01-30 13:45:05 +0530

    some more changes done

--- assignment1/library/stamp.cpp
+++ assignment1/library/stamp.cpp
@@ -15,7 +15,7 @@ struct thread_args2{
 };
 
 struct thread_args3{
-    int no_of_times, low, high, size, outer, inner, offset1, offset2, stride1, stride2;
+    int low, high, size;
     std::function<void(int, int)> input_fun;
 };
 
@@ -34,27 +34,12 @@ void *thread_func2(void *ptr){
 }
 
 void *thread_func3(void *ptr){
-
         
-        for (int i = ((thread_args3 *) ptr)->low; i < ((thread_args3 *) ptr)->high; i++){
-            ((thread_args3 *) ptr)->input_fun(i/((thread_args3 *) ptr)->size, i%((thread_args3 *) ptr)->size);
-        }
-        // int count = 0;
-        // printf("stride1 %d\n", ((thread_args3 *) ptr)->stride1);
-        // printf("stride2 %d\n", ((thread_args3 *) ptr)->stride2);
-        // for (int i = 0; i <= ((thread_args3 *) ptr)->outer; i += ((thread_args3 *) ptr)->stride1){
-            
-        //     for (int j = 0; j < ((thread_args3 *) ptr)->inner; j += ((thread_args3 *) ptr)->stride2){
-        //         // printf("ok %d %d\n", count, j);
-        //         printf("\ni: %d j : %d\n", (((thread_args3 *) ptr)->offset1) + i, (((thread_args3 *) ptr)->offset2) + j);
-        //         ((thread_args3 *) ptr)->input_fun((((thread_args3 *) ptr)->offset1) + i, (((thread_args3 *) ptr)->offset2) + j);
-        //         count++;
-        //         if (count == ((thread_args3 *) ptr)->no_of_times) break;
-        //     }
-        //     if (count == ((thread_args3 *) ptr)->no_of_times) break;
-        // }
-        return NULL;
+    for (int i = ((thread_args3 *) ptr)->low; i < ((thread_args3 *) ptr)->high; i++){
+        ((thread_args3 *) ptr)->input_fun(i/((thread_args3 *) ptr)->size, i%((thread_args3 *) ptr)->size);
     }
+    return NULL;
+}
 
 void stamp::execute_tuple(std::function<void()> &&lambda1, std::function<void()> &&lambda2){
 
@@ -99,7 +84,7 @@ void stamp::parallel_for(int low, int high, int stride, std::function<void(int)>
 }
 
 void stamp::parallel_for(int high, std::function<void(int)> &&lambda, int numThreads){
-    parallel_for(0, high, 1, (std::function<void(int)>)lambda, numThreads);
+    stamp::parallel_for(0, high, 1, (std::function<void(int)>)lambda, numThreads);
 }
 
 void stamp::parallel_for(int low1, int high1, int stride1, int low2, int high2, int stride2,
@@ -111,25 +96,28 @@ void stamp::parallel_for(int low1, int high1, int stride1, int low2, int high2, 
     thread = (pthread_t *) malloc (numThreads*sizeof(pthread_t));
     args = (thread_args3 *) malloc (numThreads*sizeof(thread_args3));
 
-    int t = 0, offset = 0;
+    int t = 0, offset = 0, n;
     int total_tasks = (ceil)((double)((high1-low1)*(high2-low2)/(stride1*stride2)));
     for (int i = 0; i < numThreads; i++){
         
-        (&args[i])->no_of_times = (ceil)((double)(total_tasks - offset)/(numThreads-t));
+        n = (ceil)((double)(total_tasks - offset)/(numThreads-t));
         (&args[i])->low = offset;
         (&args[i])->input_fun = lambda;
-        (&args[i])->high = (&args[i])->low + (&args[i])->no_of_times;
+        (&args[i])->high = (&args[i])->low + n;
         (&args[i])->size = high1 - low1;
-        offset += (&args[i])->no_of_times;
-        printf("no_of_times %d low %d high %d offset %d\n", (&args[i])->no_of_times, (&args[i])->low, (&args[i])->high, offset);
+        offset += n;
+        // printf("no_of_times %d low %d high %d offset %d\n", (&args[i])->no_of_times, (&args[i])->low, (&args[i])->high, offset);
 
         pthread_create(&thread[i], NULL, thread_func3, (void*) &args[i]);
         t++;
         if (offset >= total_tasks) break;
     }
-    printf("\n");
     
     for (int i = 0; i < numThreads; i++){
         pthread_join(thread[i], NULL);
     }
+}
+
+void parallel_for(int high1, int high2, std::function<void(int, int)> &&lambda, int numThreads){
+    stamp::parallel_for(0, high1, 1, 0, high2, 1, (std::function<void(int, int)>) lambda, numThreads);
 }
